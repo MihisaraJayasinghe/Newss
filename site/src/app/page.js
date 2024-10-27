f55d27f080a2eb39bd8aa04c16154c3a80b2abad
Pasan Mihisara <99629832+MihisaraJayasinghe@users.noreply.github.com>
2024-10-27 14:25:52 +0530

    s

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -50,6 +50,10 @@ export default function NewsPage() {
     (item) => item.tag.includes('Pradana Puwath') && item.stype !== 'pinned'
   );
 
+  const nonPinnedKridaPuwathNews = newsItems.filter(
+    (item) => item.tag.includes('Krida Puwath') && item.stype !== 'pinned'
+  );
+
   const oneDayAgo = dayjs().subtract(1, 'day');
   const recentWigasaPuwathNews = newsItems.filter((item) =>
     dayjs(item.createdAt).isAfter(oneDayAgo)
@@ -77,18 +81,27 @@ export default function NewsPage() {
             isLeftSidebarOpen ? 'block' : 'hidden lg:block'
           } bg-gray-200 flex flex-col h-full ml-2 mr-2 mt-2 lg:ml-5 lg:mr-5 lg:mt-5`}
         >
+          {/* Unusum Puwath Section */}
           <div className="p-4 bg-gray-200 flex-1 ml-2 mr-2 lg:ml-5 lg:mr-5 rounded-lg overflow-auto max-h-[2000px]">
             <h2 className="text-xl lg:text-2xl font-bold mb-2 lg:mb-4">Unusum Puwath</h2>
             <Sidebar newsItems={newsItems.filter(item => item.tag.includes('Unusum Puwath'))} />
           </div>
+
+          {/* Deshiya Puwath Section */}
           <div className="p-4 bg-gray-200 flex-1 m-2 ml-2 lg:m-5 lg:ml-5 rounded-lg overflow-auto max-h-[2000px]">
             <h2 className="text-xl lg:text-2xl font-bold mb-2 lg:mb-4">Deshiya Puwath</h2>
             <Sidebar newsItems={newsItems.filter(item => item.category.includes('deshiya'))} />
           </div>
+
+          {/* Krida Puwath Section */}
+          <div className="p-4 bg-gray-200 flex-1 m-2 ml-2 lg:m-5 lg:ml-5 rounded-lg overflow-auto max-h-[2000px]">
+            <h2 className="text-xl lg:text-2xl font-bold mb-2 lg:mb-4">Krida Puwath</h2>
+            <Sidebar newsItems={newsItems.filter(item => item.tag.includes('Krida Puwath'))} />
+          </div>
         </div>
 
         {/* Main Content */}
-        <div className="flex-1 bg-gray-200 p-2 lg:p-4 mt-2 lg:mt-5 flex flex-col justify-end">
+        <div className="flex-1 bg-gray-200 p-2     lg:p-4 mt-2 lg:mt-5 flex flex-col justify-end">
           {/* Pinned News Section */}
           {pinnedNewsItem && (
             <div className="mb-4 lg:mb-6 p-4 lg:p-6 bg-white border-4 border-blue-500 rounded-lg shadow-md max-h-[300px] lg:max-h-[400px] overflow-auto m-2 lg:m-5">
@@ -136,6 +149,8 @@ export default function NewsPage() {
             <NewsSection newsItems={nonPinnedPradanaPuwathNews} />
           </div>
 
+          
+
           {/* Wigasa Puwath Section */}
           {recentWigasaPuwathNews.length > 0 && (
             <div
@@ -161,7 +176,7 @@ export default function NewsPage() {
             id="videos-puwaths"
             className="border-2 border-black p-4 lg:p-6 bg-white rounded-md shadow-md max-h-[300px] lg:max-h-[400px] overflow-auto m-2 lg:m-5"
           >
-            <h2 className="text-xl lg:text-2xl font-bold mb-2 lg:mb-4">videos පුවත්</h2>
+            <h2 className="text-xl lg:text-2xl font-bold mb-2 lg:mb-4">Videos පුවත්</h2>
             <ul>
               {newsItems.map(
                 (news) =>
@@ -190,16 +205,22 @@ export default function NewsPage() {
               <Link key={index} href={`/newsdetail/${item._id}`} passHref>
                 <div className="mb-2 lg:mb-4 cursor-pointer hover:bg-gray-200 p-2 lg:p-4 rounded">
                   <h4 className="font-semibold text-xs lg:text-sm mb-1 lg:mb-2">{item.title}</h4>
-                  <p className="text-gray-600 text-xs lg:text-sm">
-                    {item.content.substring(0, 100)}...
-                  </p>
-                  {item.imageUrl && (
-                    <img
-                      src={item.imageUrl}
-                      alt={item.title}
-                      className="w-full h-16 lg:h-24 object-cover rounded-md mb-1 lg:mb-2"
-                    />
-                  )}
+                  <div className='flex'>
+                    <div className='w-1/2 h-full pr-2 pt-2'>
+                      {item.imageUrl && (
+                        <img
+                          src={item.imageUrl}
+                          alt={item.title}
+                          className="w-full   lg:h-24 object-cover rounded-md mb-1 lg:mb-2"
+                        />
+                      )}
+                    </div>
+                    <div className='w-1/2   overflow-scroll p-2'>
+                      <p className="text-gray-600    text-xs text-justify lg:text-xs">
+                        {item.content.substring(0, 100)}...
+                      </p>
+                    </div>
+                  </div>
                   <a href={item.link || '#'} className="text-blue-500 text-xs lg:text-sm">
                     Read more
                   </a>
